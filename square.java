5419ccfa0b92a9404da015993eecd2ad5758ef1f
Nivedita (Nivi) Biswas <nivedita10012006@gmail.com>
2025-01-07 20:20:07 +0530

    Create square.java

---
+++ square.java
@@ -0,0 +1,11 @@
+class Square {
+    public static void main(String[] args) {
+        int number = 5;  // the number to square
+        
+        // Calculate square using simple multiplication
+        int square = number * number;
+        
+        // Display result
+        System.out.println("Square of " + number + " is: " + square);
+    }
+} 
